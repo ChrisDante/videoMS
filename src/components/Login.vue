cfc22397527071dd04e990e2d9f262739ce0ea28
ChrisDante <git@battles.xyz>
2017-10-19 16:53:01 +0800

    login add pic id

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,20 @@
 <template>
    <div class="login">
-        <mu-auto-complete v-model="username" hintText="Please input your username" required></mu-auto-complete>
-        <mu-auto-complete v-model="password" hintText="Please input your password" required></mu-auto-complete>
+     <div class="container">
+        <mu-auto-complete v-model="username" hintText="请输入用户名" required fullWidth></mu-auto-complete>
+        <br>
+        <mu-auto-complete v-model="password" hintText="请输入密码" required fullWidth></mu-auto-complete>
+        <br>
+        <div class="picContainer">
+          <div>
+            <mu-auto-complete class="picans" v-model="picans" hintText="请输入验证码" required fullWidth></mu-auto-complete>                    
+          </div>
+          <img :src="$data.imgUrl" alt="正在加载验证码" @click="fetchData()" width="74" height="30">
+        </div>
         <br/>
-        <mu-raised-button label="Log in" @click="login()" fullWidth primary/>    
+        <p class="error">{{ error }}</p>
+     </div>
+    <mu-raised-button label="Log in" @click="login()" fullWidth primary/>
    </div>
 </template>
 
@@ -13,13 +24,24 @@ export default {
   data() {
     return {
       username: '',
-      password: ''
+      password: '',
+      error: '',
+      imgUrl: null,
+      picid: '',
+      picans: ''
     } 
+  },
+  mounted: function(){
+    this.fetchData();
+  },
+  watch: {
+    // imgUrl: 'fetchData'
   },
   methods:{
     login: function(){
-      if(this.username && this.password){
-        this.$http.get('/xx', {"username": this.username, "password": this.password}).then(
+      if(this.username && this.password && this.picans){
+        this.error = '';
+        this.$http.post('/192.168.123.34:8080', {"username": this.username, "password": this.password, "picans": this.picans}).then(
           res => {
             console.log(res)
           },
@@ -27,16 +49,47 @@ export default {
             console.error(res)
           }
       )
+      }else if(!this.username){
+        this.error = '请输入用户名';
+      }else if(!this.password){
+        this.error = '请输入密码';
+      }else if(!this.picans){
+        this.error = '请输入验证码';
       }
-      else{
-        console.log('more params are needed');
+    },
+    fetchData: function(){
+      var xhr = new XMLHttpRequest()
+      var self = this;
+      xhr.open('GET', 'http://192.168.123.34:8080/v1/mv/user/valcode')
+      xhr.onload = function () {
+        self.imgUrl = JSON.parse(xhr.responseText).data.src;
+        self.picid = JSON.parse(xhr.responseText).data.picid;        
       }
+      xhr.send()
     }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style>
+  .container{
+    width: 356px;
+    display: inline-block;
+    text-align: center;
+  }
+  p.error{
+    color: red;
+  }
+  .picContainer{
+    display: inline-block;
+    width: 100%;
+  }
+  .picContainer > div{
+    width: 256px;
+    float: left;
+  }
+  .mu-auto-complete .mu-text-field-hint{
+    text-align: left !important;
+  }
 </style>
